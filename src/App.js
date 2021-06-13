36287194278f65edfa2e4c4ce53e5f03064a11d9
Mahadi001 <sadat.himel@gmail.com>
2021-06-13 20:17:28 +0600

    update recipes api

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 
 const App = () => {
 
-  const APP_ID = "36483bb2";
-  const APP_KEY = "b263639c38e1cac8c5849451cffb232c";
-  const ExampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-
- const [counter, setCounter] = useState(0);
+  const APP_ID ="13da73c0";
+  const APP_KEY ="bd93438cb61090829657d861f0109ef2";
+  
+  const [recipes,setRecipes] = useState([]);
 
  useEffect(()=>{
-  console.log('Effect has been run');
- });
+  getRecipes();
+ }, []);
+
+ const getRecipes = async () => {
+   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+   const data = await response.json();
+   setRecipes(data.hits);
+ }
 
   return (
     <div className = "App">
@@ -20,7 +24,6 @@ const App = () => {
         <input className = "search-bar" type="text" />
         <button className = "search-button" type="submit">Search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)} >{counter}</h1>
     </div>
   )
 }
